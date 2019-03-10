05ac79f9b65a5a9dc040da8ed909a6a718350c04
Nakata <kojinktganbaoru2012@gmail.com>
2019-03-10 20:49:09 +0900

    update ランダム文字列反映

--- test/js/test.js
+++ test/js/test.js
@@ -1,6 +1,17 @@
 
+$(document).ready( function(){
+	show_random_text();
+});
+
 function show_result(result_text){
-	result_td = $('#result'); 
-	result_td.text(result_text);
+	elem = $('#result'); 
+	elem.text(result_text);
 }
 
+function show_random_text(){
+	
+	random_text = Math.random().toString(36).slice(-8);
+	
+	elem = $('#random_text'); 
+	elem.text();
+}
